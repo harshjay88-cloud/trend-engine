84cd65fa3acaa6e7a8468869bd5fce77361acd1f
harshjay88-cloud <harshjay88@gmail.com>
2026-03-10 11:38:32 +0100

    Update youtube_suggest.py

--- collectors/youtube_suggest.py
+++ collectors/youtube_suggest.py
@@ -1,1 +1,12 @@
+import requests
 
+def get_suggestions(query):
+    url=f"https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q={query}"
+    r=requests.get(url).json()
+    return r[1]
+
+if __name__ == "__main__":
+    keywords = get_suggestions("music")
+
+    for k in keywords:
+        print(k)
